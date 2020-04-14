023b7b4aa4666a1bfa43efff90c5567cf44e3ba9
Ian Gow <iandgow@gmail.com>
2020-04-14 16:57:45 -0400

    Fixed __init__.py.
    - Moved __init__.py to project directory.
    - Added import statements.

--- __init__.py
+++
@@ -1,4 +0,0 @@
-name = "wrds2pg"
-
-import wrds2pg.wrds2pg as wrds2pg
-from wrds2pg.wrds2pg import wrds_update
---
+++ wrds2pg/__init__.py
@@ -0,0 +1,5 @@
+name = "wrds2pg"
+
+from wrds2pg.wrds2pg import wrds_update, run_file_sql, 
+from wrds2pg.wrds2pg import make_engine, get_process, wrds_process_to_pg
+from wrds2pg.wrds2pg import wrds_id, set_table_comment
