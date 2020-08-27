368aff750b09c9237a8254b3843124b326431b07
Ian Gow <iandgow@gmail.com>
2020-08-26 21:32:59 -0400

    Import get_file_sql.

--- wrds2pg/__init__.py
+++ wrds2pg/__init__.py
@@ -2,4 +2,4 @@ name = "wrds2pg"
 
 from wrds2pg.wrds2pg import wrds_update, run_file_sql, get_modified_str
 from wrds2pg.wrds2pg import make_engine, get_process, wrds_process_to_pg
-from wrds2pg.wrds2pg import wrds_id, set_table_comment
+from wrds2pg.wrds2pg import wrds_id, set_table_comment, get_file_sql
