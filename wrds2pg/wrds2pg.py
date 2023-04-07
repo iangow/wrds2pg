20c4299e57ae256aa7fcfb59a9825f390a14a537
Ian Gow <iandgow@gmail.com>
2023-04-08 07:29:32 +1000

    Change WRDS URL for 2FA.

--- wrds2pg/wrds2pg.py
+++ wrds2pg/wrds2pg.py
@@ -25,7 +25,7 @@ def get_process(sas_code, wrds_id=None, fpath=None):
         returns result as pipe on stdout."""
         client.load_system_host_keys()
         client.set_missing_host_key_policy(paramiko.WarningPolicy())
-        client.connect('wrds-cloud.wharton.upenn.edu',
+        client.connect('wrds-cloud-sshkey.wharton.upenn.edu',
                        username=wrds_id, compress=False)
         command = "qsas -stdio -noterminal"
         stdin, stdout, stderr = client.exec_command(command)
